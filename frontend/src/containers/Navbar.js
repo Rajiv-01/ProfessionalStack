c47357abceba25c78bd61f0cb888030a8a45bc59
Rajiv Sharma <rajivsharma4180@gmail.com>
2022-02-06 02:30:19 +0530

    basic navbar done

--- frontend/src/containers/Navbar.js
+++ frontend/src/containers/Navbar.js
@@ -2,10 +2,57 @@ import React from 'react';
 import { Avatar, TextField } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Select from 'react-select';
 
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+	const options = [
+		{ value: 'channel1', label: 'Channel1' },
+		{ value: 'channel2', label: 'Channel2' },
+		{ value: 'channel3', label: 'Channel3' },
+	];
+	const customStyles = {
+		noOptionsMessage: (provided) => ({
+			...provided,
+			color: 'white',
+		}),
+		option: (provided, state) => ({
+			...provided,
+			backgroundColor: state.isFocused
+				? 'rgba(50,50,50,0.9)'
+				: 'rgba(120,120,120,0.9)',
+		}),
+		control: (provided) => ({
+			...provided,
+			backgroundColor: 'rgba(120,120,120,0.9)',
+			textDecoration: 'none',
+			width: '130px',
+			border: 'none',
+			height: '30px',
+		}),
+		placeholder: (provided) => ({
+			...provided,
+			color: 'white',
+		}),
+		input: (provided) => ({
+			...provided,
+			color: 'white',
+		}),
+		menu: (provided) => ({
+			...provided,
+			backgroundColor: 'rgba(150,150,150,0.9)',
+			color: 'white',
+		}),
+		singleValue: (provided) => ({
+			...provided,
+			color: 'white',
+		}),
+		dropdownIndicator: (provided) => ({
+			...provided,
+			color: 'white',
+		}),
+	};
 	return (
 		<>
 			<div className='navbar'>
@@ -22,13 +69,14 @@ function Navbar() {
 						<input type='text' placeholder='Search' className='search_bar' />
 					</form>
 				</div>
-				<select name='channels' id='channels'>
-					<option value='Channel1'>Channel1</option>
-					<option value='Channel2'>Channel2</option>
-					<option value='Channel3'>Channel3</option>
-					<option value='Channel4'>Channel4</option>
-				</select>
-				<button> Join </button>
+				<div className='channels'>
+					<Select
+						options={options}
+						styles={customStyles}
+						placeholder='Channels'
+					></Select>
+				</div>
+				<button id='join_button'> Join </button>
 			</div>
 		</>
 	);
